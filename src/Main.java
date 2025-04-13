ecedf73f987ada1473f23df03fed93b8fc87f123
Miha <shmihail85@yandex.ru>
2025-04-13 21:31:59 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Main {
+    private final static Random RANDOM=new Random();
+
+    private final static Employee[] EMPLOYEES=new Employee[10];
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
